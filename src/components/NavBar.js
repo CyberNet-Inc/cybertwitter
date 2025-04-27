554b1c792f0c735c3cfe0a6e6da1fd6b059ed8c8
CyberNet Labs Inc. <rupture_general_0a@icloud.com>
2025-04-27 17:51:27 +0200

    Add NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/profile">Profile</Link>
+    <Link to="/login">Login</Link>
+  </nav>
+);
+
+export default NavBar;
